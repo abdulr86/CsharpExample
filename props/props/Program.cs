1e8646547eb93af9e6ab318e6892fe1ce60cdbb0
abdul86 <bazer786@gmail.com>
2024-12-24 17:24:23 +0530

    commit

---
+++ props/props/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace props
+{
+    class Mydemoprops
+    {
+        public static int count;
+
+        public Mydemoprops()
+        {
+            count++;
+        }
+        public static int  propName
+        {
+            get { return count; }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mydemoprops mydemoprops = new Mydemoprops();
+            Mydemoprops mydemoprops1 = new Mydemoprops();
+            Mydemoprops mydemoprops2 = new Mydemoprops();
+
+            Console.WriteLine(Mydemoprops.propName);
+
+        }
+    }
+}
