a2296b08d85e8781e2e13fac93f18156b387ad46
abdul86 <bazer786@gmail.com>
2024-12-24 11:45:47 +0530

    commit

---
+++ polymorphism/polymorphism/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Academy academy = new Academy();
+            Academy academy2 = new Academy2();
+
+            academy.shree();
+            academy2.shree();
+
+        }
+    }
+}
---
+++ polymorphism/polymorphism/academy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphism
+{
+     class Academy
+    {
+        public virtual void shree()
+        {
+            Console.WriteLine("This is yagnik road branch");
+        }
+    }
+    class Academy2:Academy
+    {
+        public override void shree()
+        {
+            Console.WriteLine("This is univercity road branch");
+        }
+
+    }
+
+
+}
