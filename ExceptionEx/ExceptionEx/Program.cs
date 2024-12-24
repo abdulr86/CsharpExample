264b2b5ba87735125f6b17b5960643da506cbb6b
abdul86 <bazer786@gmail.com>
2024-12-24 11:36:56 +0530

    commit

---
+++ ExceptionEx/ExceptionEx/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionEx
+{
+    internal class Program
+    {
+        public static void chekcarray()
+        {
+            int[] myNumbers = { 1, 2, 3 };
+            Console.WriteLine(myNumbers[10]);
+        }
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                chekcarray();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }            
+            finally
+            {
+                Console.WriteLine("File running..");
+            }
+        }
+    }
+}
