b5d0868acd2964bff84de0c0d1d6f1a3f10bcf19
abdul86 <bazer786@gmail.com>
2024-12-24 12:34:36 +0530

    commit

---
+++ Interface/Interface/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    class Academy
+    {
+        //hi
+        int collection1 = 800000;
+        int collection12 = 400000;
+        static void Main(string[] args)
+        {
+            Academy total = new Academy();
+            BigBranch mybranch = new BigBranch();
+
+
+            Console.WriteLine("Both Branch Collection is " + (mybranch.yagnikroad(total.collection1) + mybranch.univercityroad(total.collection12)));
+        }
+    }
+}
---
+++ Interface/Interface/shree.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    interface branch1
+    {
+        int yagnikroad(int collection1);
+    }
+    interface branch2
+    {
+        int univercityroad(int collection12);
+    }
+    class BigBranch : branch1, branch2
+    {
+        public int yagnikroad(int collection1)
+        {
+            return collection1;
+        }
+        public int univercityroad(int collection12)
+        {
+            return collection12;
+        }
+    }
+}
