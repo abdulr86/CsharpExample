ad9f5abe5ddf0c9e8f4aeff2d2fa86354ff7cfff
abdul86 <bazer786@gmail.com>
2024-12-24 11:47:03 +0530

    commit

---
+++ FileHendling/FileHendling/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace FileHendling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //String WriteText = "Hello world";
+            //File.WriteAllText("D:/Myfile.txt",WriteText);
+
+            String path = "D:/Myfile.txt";
+            if (File.Exists(path))
+            {
+                File.AppendAllText(path, "This is my modified Data");
+                String readText = File.ReadAllText("D:/Myfile.txt");
+                Console.WriteLine(readText);
+            }
+            else
+            {
+                Console.WriteLine("File not found");
+            }
+        }
+    }
+}
