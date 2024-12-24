6c0883959a99383ebe07ab79804676056a71dd6e
abdul86 <bazer786@gmail.com>
2024-12-24 11:17:26 +0530

    abstract

---
+++ abstractClass/abstractClass/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstractClass
+{
+    internal class Program
+    {
+        int x = 100;
+        int y = 200;
+        enum Myvalue
+        {
+            high = 300,
+        }
+        static void Main(string[] args)
+        {
+            Myvalue myvalue = Myvalue.high;
+            Program absclass = new Program();
+            CsharpClass csharpclass = new CsharpClass();            
+            Console.WriteLine(csharpclass.mymaths(absclass.x, absclass.y)+myvalue);
+        }
+    }
+}
---
+++ abstractClass/abstractClass/absClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstractClass
+{
+    abstract class absClass
+    {
+
+        public abstract int mymaths(int a,int b);
+    }
+
+    class CsharpClass:absClass
+    {
+
+        public override int mymaths(int a,int b)
+        {
+            return a+b;           
+        }
+    }
+}
