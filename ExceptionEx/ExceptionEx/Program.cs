ddb5416c52c190b367c4aefef0270c07338a1c4b
abdul86 <bazer786@gmail.com>
2024-12-25 11:48:08 +0530

    commit

--- ExceptionEx/ExceptionEx/Program.cs
+++ ExceptionEx/ExceptionEx/Program.cs
@@ -3,26 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ExceptionEx
 {
+    class MyCustomClass:Exception
+    {
+        public MyCustomClass(String msg):base(msg) 
+        {
+            throw new Exception(msg);     
+        }
+    }
+    
     internal class Program
     {
         //demo
-        public static void chekcarray()
+        public static void ChekcValue(int x,int y)
         {
-            int[] myNumbers = { 1, 2, 3 };
-            Console.WriteLine(myNumbers[10]);
+            try
+            {
+                //int[] myNumbers = { 1, 2, 3 };
+                //Console.WriteLine(myNumbers[10]);
+                if (y==0)
+                {
+                    throw new DivideByZeroException();
+                }
+                if(x>50)
+                {
+                    throw new MyCustomClass("This is Custorm Exception");
+                }
+                Console.WriteLine(x/y);
+            }
+            catch
+            {
+                throw;
+            }
         }
         static void Main(string[] args)
         {
-
+            
             try
             {
-                chekcarray();
+                int a = 12;
+                int b = 12;
+                ChekcValue(a,b);
             }
             catch (Exception e)
             {
+                //File.WriteAllText("D:/Myfile.txt", $"{DateTime.Now} => Error message : {e.Message} \n Stacktrace : {e.StackTrace.ToString()}");
                 Console.WriteLine(e.Message);
             }            
             finally
